e32593eb91bd18062935a09faf03eff7f9516710
Michele Beltrame <mb@italpro.net>
2018-06-05 23:11:37 +0200

    nice

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,37 @@
 struct Urbe {
-  name  : String
+    nomen : String
+}
+
+struct UrbeInVia {
+    urbe      : Urbe,
+    miliarium : u32
+}
+
+struct Via {
+    nomen           : String,
+    urbes_in_via    : Vec<UrbeInVia>
 }
 
 fn main() {
-    let urbi = [Urbe {name:"Roma".to_string()}, Urbe {name:"Mediolanum".to_string()}];
+    let vias = vec![
+        Via { nomen: "Flaminia".to_string(), urbes_in_via : vec![
+            UrbeInVia { urbe: Urbe { nomen: "Ostia".to_string() }, miliarium: 0 },
+            UrbeInVia { urbe: Urbe { nomen: "Roma".to_string() }, miliarium: 1 },
+            UrbeInVia { urbe: Urbe { nomen: "Spoletum".to_string() }, miliarium: 10 },
+            UrbeInVia { urbe: Urbe { nomen: "Ariminium".to_string() }, miliarium: 20 }
+        ] },
+        Via { nomen: "Aemilia".to_string(), urbes_in_via : vec![
+            UrbeInVia { urbe: Urbe { nomen: "Ariminium".to_string() }, miliarium: 0 },
+            UrbeInVia { urbe: Urbe { nomen: "Bononia".to_string() }, miliarium: 10 },
+            UrbeInVia { urbe: Urbe { nomen: "Parma".to_string() }, miliarium: 20 },
+            UrbeInVia { urbe: Urbe { nomen: "Placentia".to_string() }, miliarium: 30 }
+        ] },
+    ];
 
-    for urbe in &urbi {
-      println!("{}", urbe.name);
+    for via in &vias {  // Why the &?
+        println!("Via {}", via.nomen);
+        for urbe_in_via in &via.urbes_in_via {
+          println!("    {} [{}]", urbe_in_via.urbe.nomen, urbe_in_via.miliarium);
+        }
     }
 }
