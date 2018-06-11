9a4eb89c05ef816d44b38005004afa31217524f0
Michele Beltrame <mb@italpro.net>
2018-06-11 23:05:41 +0200

    On

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,26 @@ mod viae;
 fn main() {
     let vrs = viae::get_viae();
 
-    find_path(vrs, "Roma".to_string(), "Pisae".to_string());
+    let start_urbe = find_urbe(&vrs, "Roma".to_string()).expect("Starting urbe not found");
+    println!("START: {} [miliarum: {}]", start_urbe.nomen, start_urbe.miliarium);
+
+    let dest_urbe = find_urbe(&vrs, "Ariminium".to_string()).expect("Destination urbe not found");
+    println!("DESTINATION: {} [miliarum: {}]", dest_urbe.nomen, dest_urbe.miliarium);
 }
 
-fn find_path(vrs: Vec<viae::Via>, start: String, finish: String) {
-    println!("Start: {}, Finish: {}", start, finish);
-
-    for via in &vrs {
-        for urbe_in_via in &via.urbes_in_via {
-            if urbe_in_via.nomen == start {
-                println!("{} [{}]", via.nomen, urbe_in_via.miliarium);
+// TODO: accept &str instead of string, but lifetimes need to be defined - one I understand them
+fn find_urbe(vrs: &Vec<viae::Via>, nomen: String) -> Option<&viae::Urbe> {
+    for via in vrs { // "via" is a borrowed reference
+        for urbe in &via.urbes { // "urbe" is a borrowed reference
+            if urbe.nomen == nomen {
+                return Some(urbe);
+                //println!("{} [{}]", via.nomen, urbe.miliarium);
             }
         }
     }
+    None
 }
+
+// fn find_path(start_via: &Via) {
+//      println!("{} [{}]", via.nomen, urbe.miliarium);
+//}
--- src/viae.rs
+++ src/viae.rs
@@ -1,36 +1,32 @@
-// struct Urbe {
-//     nomen : String
-// }
-
-pub struct UrbeInVia {
+pub struct Urbe {
     pub nomen     : String,
     pub miliarium : u32
 }
 
 pub struct Via {
-    pub nomen           : String,
-    pub urbes_in_via    : Vec<UrbeInVia>
+    pub nomen     : String,
+    pub urbes     : Vec<Urbe>
 }
 
 pub fn get_viae() -> Vec<Via> {
     let viae = vec![
-        Via { nomen: "Flaminia".to_string(), urbes_in_via : vec![
-            UrbeInVia { nomen: "Ostia".to_string(), miliarium: 0 },
-            UrbeInVia { nomen: "Roma".to_string(), miliarium: 1 },
-            UrbeInVia { nomen: "Spoletum".to_string(), miliarium: 10 },
-            UrbeInVia { nomen: "Ariminium".to_string(), miliarium: 20 }
+        Via { nomen: "Flaminia".to_string(), urbes : vec![
+            Urbe { nomen: "Ostia".to_string(), miliarium: 0 },
+            Urbe { nomen: "Roma".to_string(), miliarium: 1 },
+            Urbe { nomen: "Spoletum".to_string(), miliarium: 10 },
+            Urbe { nomen: "Ariminium".to_string(), miliarium: 20 }
         ] },
-        Via { nomen: "Aemilia".to_string(), urbes_in_via : vec![
-            UrbeInVia { nomen: "Ariminium".to_string(), miliarium: 0 },
-            UrbeInVia { nomen: "Bononia".to_string(), miliarium: 10 },
-            UrbeInVia { nomen: "Parma".to_string(), miliarium: 20 },
-            UrbeInVia { nomen: "Placentia".to_string(), miliarium: 30 }
+        Via { nomen: "Aemilia".to_string(), urbes : vec![
+            Urbe { nomen: "Ariminium".to_string(), miliarium: 0 },
+            Urbe { nomen: "Bononia".to_string(), miliarium: 10 },
+            Urbe { nomen: "Parma".to_string(), miliarium: 20 },
+            Urbe { nomen: "Placentia".to_string(), miliarium: 30 }
         ] },
-        Via { nomen: "Aurelia".to_string(), urbes_in_via : vec![
-            UrbeInVia { nomen: "Roma".to_string(), miliarium: 0 },
-            UrbeInVia { nomen: "Populonium".to_string(), miliarium: 10 },
-            UrbeInVia { nomen: "Pisae".to_string(), miliarium: 20 },
-            UrbeInVia { nomen: "Luna".to_string(), miliarium: 30 }
+        Via { nomen: "Aurelia".to_string(), urbes : vec![
+            Urbe { nomen: "Roma".to_string(), miliarium: 0 },
+            Urbe { nomen: "Populonium".to_string(), miliarium: 10 },
+            Urbe { nomen: "Pisae".to_string(), miliarium: 20 },
+            Urbe { nomen: "Luna".to_string(), miliarium: 30 }
         ] },
     ];
 
