85358446d030117221c9b8b0b4798dbe51529bce
Michele Beltrame <mb@italpro.net>
2018-06-11 23:12:22 +0200

    tuples

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,19 @@ mod viae;
 fn main() {
     let vrs = viae::get_viae();
 
-    let start_urbe = find_urbe(&vrs, "Roma".to_string()).expect("Starting urbe not found");
-    println!("START: {} [miliarum: {}]", start_urbe.nomen, start_urbe.miliarium);
+    let (start_urbe, start_via) = find_urbe(&vrs, "Roma".to_string()).expect("Starting urbe not found");
+    println!("START: {} [via: {} - miliarum: {}]", start_urbe.nomen, start_via.nomen, start_urbe.miliarium);
 
-    let dest_urbe = find_urbe(&vrs, "Ariminium".to_string()).expect("Destination urbe not found");
-    println!("DESTINATION: {} [miliarum: {}]", dest_urbe.nomen, dest_urbe.miliarium);
+    let (dest_urbe, dest_via) = find_urbe(&vrs, "Ariminium".to_string()).expect("Destination urbe not found");
+    println!("DESTINATION: {} [via: {} - miliarum: {}]", dest_urbe.nomen, dest_via.nomen, dest_urbe.miliarium);
 }
 
 // TODO: accept &str instead of string, but lifetimes need to be defined - one I understand them
-fn find_urbe(vrs: &Vec<viae::Via>, nomen: String) -> Option<&viae::Urbe> {
+fn find_urbe(vrs: &Vec<viae::Via>, nomen: String) -> Option<(&viae::Urbe, &viae::Via)> {
     for via in vrs { // "via" is a borrowed reference
         for urbe in &via.urbes { // "urbe" is a borrowed reference
             if urbe.nomen == nomen {
-                return Some(urbe);
+                return Some((urbe, via));
                 //println!("{} [{}]", via.nomen, urbe.miliarium);
             }
         }
