604e0b395efd85c4e063d00dbd0f10489a7f2697
Michele Beltrame <mb@italpro.net>
2018-06-13 00:55:07 +0200

    finding our way

--- src/main.rs
+++ src/main.rs
@@ -3,26 +3,60 @@ mod viae;
 fn main() {
     let vrs = viae::get_viae();
 
-    let (start_urbe, start_via) = find_urbe(&vrs, "Roma".to_string()).expect("Starting urbe not found");
-    println!("START: {} [via: {} - miliarum: {}]", start_urbe.nomen, start_via.nomen, start_urbe.miliarium);
+    let start_urbe = "Roma".to_string();
+    // let start_via = find_via(&vrs, start_urbe.clone()).expect("Starting urbe not found in our viae!");
+    // println!("START: {} [via: {}]", start_urbe, start_via);
 
-    let (dest_urbe, dest_via) = find_urbe(&vrs, "Ariminium".to_string()).expect("Destination urbe not found");
-    println!("DESTINATION: {} [via: {} - miliarum: {}]", dest_urbe.nomen, dest_via.nomen, dest_urbe.miliarium);
+    let dest_urbe = "Ariminium".to_string();
+    // let dest_via = find_via(&vrs, dest_urbe.clone()).expect("Destination urbe not found un our database");
+    // println!("DESTINATION: {} [via: {}]", dest_urbe, dest_via);
+
+    let path : Vec<String> = vec![];
+    find_path(&vrs, start_urbe, dest_urbe, path);
 }
 
 // TODO: accept &str instead of string, but lifetimes need to be defined - one I understand them
-fn find_urbe(vrs: &Vec<viae::Via>, nomen: String) -> Option<(&viae::Urbe, &viae::Via)> {
-    for via in vrs { // "via" is a borrowed reference
-        for urbe in &via.urbes { // "urbe" is a borrowed reference
-            if urbe.nomen == nomen {
-                return Some((urbe, via));
-                //println!("{} [{}]", via.nomen, urbe.miliarium);
+// fn find_via(vrs: &Vec<viae::Via>, nomen: String) -> Option<(String)> {
+//     for via in vrs { // "via" is a borrowed reference
+//         for urbe in &via.urbes { // "urbe" is a borrowed reference
+//             if urbe.nomen == nomen {
+//                 return Some( via.nomen.clone() );
+//                 //println!("{} [{}]", via.nomen, urbe.miliarium);
+//             }
+//         }
+//     }
+//     None
+// }
+
+fn find_path(vrs: &Vec<viae::Via>, start_urbe: String, dest_urbe: String, mut path: Vec<String>) {
+    // Find all via which traverse the urbe
+    // TODO: maybe use a grep() to find the via
+    for via in vrs {
+        let via_len = *(&via.urbes.len());
+        for i in 0 .. via_len { // "urbe" is a borrowed reference
+            let urbe = &via.urbes[i];
+            if urbe.nomen == start_urbe {
+                println!("CUR: {} [{} - {}]", via.nomen, urbe.nomen, urbe.miliarium);
+                // Walk the via in both directions
+                if ( i < via_len-1 ) {
+                    for j in i+1 .. via_len {
+                        let next_urbe = &via.urbes[j];
+                        println!("NX: {} [{} - {}]", via.nomen, next_urbe.nomen, next_urbe.miliarium);
+                    }
+                }
+                if ( i > 0 ) {
+                    for j in (0 .. i).rev() {
+                        let prev_urbe = &via.urbes[j];
+                        println!("PV: {} [{} - {}]", via.nomen, prev_urbe.nomen, prev_urbe.miliarium);
+                    }
+                }
+                //Vec.index()
+                //return Some( via.nomen.clone() );
             }
         }
     }
-    None
+    // if path.len() < 10 {
+    //     path.push( start_urbe.clone() );
+    //     find_path(start_urbe, start_via, path);
+    // }
 }
-
-// fn find_path(start_via: &Via) {
-//      println!("{} [{}]", via.nomen, urbe.miliarium);
-//}
