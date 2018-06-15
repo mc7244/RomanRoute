a8e8bfc42b1f949fe7723d4a94324acbf4e29e94
Michele Beltrame <mb@italpro.net>
2018-06-16 00:24:44 +0200

    Not bad

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,32 @@
 mod viae;
 
+static DEBUG : usize = 1;
+
 fn main() {
     let vrs = viae::get_viae();
 
     let start_urbe = "Roma".to_string();
 
     //let dest_urbe = "Ariminium".to_string();
-    let dest_urbe = "Parma".to_string();
+    //let dest_urbe = "Parma".to_string();
+    let dest_urbe = "Mediolanum".to_string();
 
-    find_path(&vrs, start_urbe, dest_urbe, "".to_string(), 0, 0);
+    //let traversed_urbes : Vec<String> = [""]
+    find_path(&vrs, start_urbe, dest_urbe, "".to_string(), &vec!["".to_string()], 0);
 }
 
-fn find_path(vrs: &Vec<viae::Via>, start_urbe: String, dest_urbe: String, from_via: String, steps: usize, depth: usize) {
+fn find_path(
+    vrs: &Vec<viae::Via>, start_urbe: String, dest_urbe: String,
+    from_via: String, traversed_urbes: &Vec<String>, depth: usize
+) {
     if depth > 10 {
-        println!("TOO MUCH RECURSION");
+        for _dp in 0 .. depth { print!("  "); }
+        println!("!!! TOO MUCH RECURSION !!!");
         return;
     }
+
+    let steps = traversed_urbes.len();
+
     // Find all via which traverse the urbe
     // TODO: maybe use a grep() to find the via
     for via in vrs {
@@ -27,40 +38,74 @@ fn find_path(vrs: &Vec<viae::Via>, start_urbe: String, dest_urbe: String, from_v
         for i in 0 .. via_len { // "urbe" is a borrowed reference
             let urbe = &via.urbes[i];
             if urbe.nomen == start_urbe {
-                if depth > 0 {
-                    for _dp in 1 .. depth { print!("  "); }
-                    print!("-> "); 
+                if DEBUG > 0 {
+                    if depth > 0 {
+                        for _dp in 1 .. depth { print!("  "); }
+                        print!("-> "); 
+                    }
+                    println!("VIA: {} [{} - {}]", via.nomen, urbe.nomen, urbe.miliarium);
                 }
-                println!("VIA: {} [{} - {}]", via.nomen, urbe.nomen, urbe.miliarium);
+
+                let mut indent = String::new();
+                for _dp in 0 .. depth { for _si in 0 .. 3 { indent.push(' '); } }
+
                 // Walk the via in both directions
                 // TODO: use slices and iterate on them with an iterator
                 if i < via_len-1 {
                     let mut cursteps = steps;
+                    let mut curtraversed = traversed_urbes.clone();
                     for j in i+1 .. via_len {
                         let next_urbe = &via.urbes[j];
                         cursteps += 1;
-                        for _dp in 0 .. depth { print!("   "); }
-                        println!("STEP {} - NX: {} [{} - {}]", cursteps, via.nomen, next_urbe.nomen, next_urbe.miliarium);
+                        if DEBUG > 0 {
+                            println!("{}STEP {} - NX: {} [{} - {}]", indent, cursteps, via.nomen, next_urbe.nomen, next_urbe.miliarium);
+                        }
+
+                        // If we have already been in this urbe before in this path, abandon the path
+                        // otherwise we will loop
+                        let next_urbe_nomen = &next_urbe.nomen;
+                        if curtraversed.iter().position(|ref el| *el == next_urbe_nomen) != None {
+                            if DEBUG > 0 {
+                                println!("{}   already been here, abandoning path", indent);
+                            }
+                            continue;
+                        }
+
+                        curtraversed.push(next_urbe.nomen.clone());
                         if next_urbe.nomen == dest_urbe {
-                            println!("FOUND DEST!!!\n");
+                            println!("{}   !!! FOUND DEST !!!\n", indent);
                             break;
                         } else {
-                            find_path(vrs, next_urbe.nomen.clone(), dest_urbe.clone(), via.nomen.clone(), cursteps, depth+1);
+                            find_path(vrs, next_urbe.nomen.clone(), dest_urbe.clone(), via.nomen.clone(), &curtraversed, depth+1);
                         }
                     }
                 }
                 if i > 0 {
                     let mut cursteps = steps;
+                    let mut curtraversed = traversed_urbes.clone();
                     for j in (0 .. i).rev() {
                         let prev_urbe = &via.urbes[j];
                         cursteps += 1;
-                        for _dp in 0 .. depth { print!("   "); }
-                        println!("STEP {} - PV: {} [{} - {}]", cursteps, via.nomen.clone(), prev_urbe.nomen, prev_urbe.miliarium);
+                        if DEBUG > 0 {
+                            println!("{}STEP {} - PV: {} [{} - {}]", indent, cursteps, via.nomen.clone(), prev_urbe.nomen, prev_urbe.miliarium);
+                        }
+
+                        // If we have already been in this urbe before in this path, abandon the path
+                        // otherwise we will loop
+                        let prev_urbe_nomen = &prev_urbe.nomen;
+                        if curtraversed.iter().position(|ref el| *el == prev_urbe_nomen) != None {
+                            if DEBUG > 0 {
+                                println!("{}   already been here, abandoning path", indent);
+                            }
+                            continue;
+                        }
+
+                        curtraversed.push(prev_urbe.nomen.clone());
                         if via.nomen == dest_urbe {
-                            println!("FOUND DEST!!!\n");
+                            println!("{}   !!! FOUND DEST !!!\n", indent);
                             break;
                         } else {
-                            find_path(vrs, prev_urbe.nomen.clone(), dest_urbe.clone(), via.nomen.clone(), cursteps, depth+1);
+                            find_path(vrs, prev_urbe.nomen.clone(), dest_urbe.clone(), via.nomen.clone(), &curtraversed, depth+1);
                         }
                     }
                 }
--- src/viae.rs
+++ src/viae.rs
@@ -10,23 +10,38 @@ pub struct Via {
 
 pub fn get_viae() -> Vec<Via> {
     let viae = vec![
-        Via { nomen: "Flaminia".to_string(), urbes : vec![
-            Urbe { nomen: "Ostia".to_string(), miliarium: 0 },
-            Urbe { nomen: "Roma".to_string(), miliarium: 1 },
-            Urbe { nomen: "Spoletum".to_string(), miliarium: 10 },
-            Urbe { nomen: "Ariminium".to_string(), miliarium: 20 }
-        ] },
         Via { nomen: "Aemilia".to_string(), urbes : vec![
             Urbe { nomen: "Ariminium".to_string(), miliarium: 0 },
             Urbe { nomen: "Bononia".to_string(), miliarium: 10 },
             Urbe { nomen: "Parma".to_string(), miliarium: 20 },
-            Urbe { nomen: "Placentia".to_string(), miliarium: 30 }
+            Urbe { nomen: "Placentia".to_string(), miliarium: 30 },
+        ] },
+        Via { nomen: "Cassia".to_string(), urbes : vec![
+            Urbe { nomen: "Roma".to_string(), miliarium: 0 },
+            Urbe { nomen: "Arretium".to_string(), miliarium: 10 },
+            Urbe { nomen: "Florentia".to_string(), miliarium: 20 },
+            Urbe { nomen: "Luna".to_string(), miliarium: 30 },
+        ] },
+        Via { nomen: "Claudia Augusta".to_string(), urbes : vec![
+            Urbe { nomen: "Florentia".to_string(), miliarium: 0 },
+            Urbe { nomen: "Bononia".to_string(), miliarium: 10 },
+            Urbe { nomen: "Verona".to_string(), miliarium: 20 },
+            Urbe { nomen: "Aug. Vindelicum".to_string(), miliarium: 30 },
+            Urbe { nomen: "Cambodunum".to_string(), miliarium: 30 },
+            Urbe { nomen: "Curia".to_string(), miliarium: 30 },
+            Urbe { nomen: "Mediolanum".to_string(), miliarium: 30 },
         ] },
         Via { nomen: "Aurelia".to_string(), urbes : vec![
             Urbe { nomen: "Roma".to_string(), miliarium: 0 },
             Urbe { nomen: "Populonium".to_string(), miliarium: 10 },
             Urbe { nomen: "Pisae".to_string(), miliarium: 20 },
-            Urbe { nomen: "Luna".to_string(), miliarium: 30 }
+            Urbe { nomen: "Luna".to_string(), miliarium: 30 },
+        ] },
+        Via { nomen: "Flaminia".to_string(), urbes : vec![
+            Urbe { nomen: "Ostia".to_string(), miliarium: 0 },
+            Urbe { nomen: "Roma".to_string(), miliarium: 1 },
+            Urbe { nomen: "Spoletum".to_string(), miliarium: 10 },
+            Urbe { nomen: "Ariminium".to_string(), miliarium: 20 }
         ] },
     ];
 
