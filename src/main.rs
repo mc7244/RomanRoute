9b0e1805c9ec0c73710676fb0b125295e8e94fb7
Michele Beltrame <mb@italpro.net>
2018-06-15 20:26:44 +0200

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -4,30 +4,12 @@ fn main() {
     let vrs = viae::get_viae();
 
     let start_urbe = "Roma".to_string();
-    // let start_via = find_via(&vrs, start_urbe.clone()).expect("Starting urbe not found in our viae!");
-    // println!("START: {} [via: {}]", start_urbe, start_via);
 
     //let dest_urbe = "Ariminium".to_string();
     let dest_urbe = "Parma".to_string();
-    // let dest_via = find_via(&vrs, dest_urbe.clone()).expect("Destination urbe not found un our database");
-    // println!("DESTINATION: {} [via: {}]", dest_urbe, dest_via);
 
-    //let mut path : Vec<String> = vec![];
     find_path(&vrs, start_urbe, dest_urbe, "".to_string(), 0, 0);
 }
-
-// TODO: accept &str instead of string, but lifetimes need to be defined - one I understand them
-// fn find_via(vrs: &Vec<viae::Via>, nomen: String) -> Option<(String)> {
-//     for via in vrs { // "via" is a borrowed reference
-//         for urbe in &via.urbes { // "urbe" is a borrowed reference
-//             if urbe.nomen == nomen {
-//                 return Some( via.nomen.clone() );
-//                 //println!("{} [{}]", via.nomen, urbe.miliarium);
-//             }
-//         }
-//     }
-//     None
-// }
 
 fn find_path(vrs: &Vec<viae::Via>, start_urbe: String, dest_urbe: String, from_via: String, steps: usize, depth: usize) {
     if depth > 10 {
@@ -41,11 +23,14 @@ fn find_path(vrs: &Vec<viae::Via>, start_urbe: String, dest_urbe: String, from_v
         if via.nomen == from_via {
             continue;
         }
-        let via_len = *(&via.urbes.len());
+        let via_len = via.urbes.len();
         for i in 0 .. via_len { // "urbe" is a borrowed reference
             let urbe = &via.urbes[i];
             if urbe.nomen == start_urbe {
-                for _dp in 0 .. depth { print!("  "); }
+                if depth > 0 {
+                    for _dp in 1 .. depth { print!("  "); }
+                    print!("-> "); 
+                }
                 println!("VIA: {} [{} - {}]", via.nomen, urbe.nomen, urbe.miliarium);
                 // Walk the via in both directions
                 // TODO: use slices and iterate on them with an iterator
@@ -54,7 +39,7 @@ fn find_path(vrs: &Vec<viae::Via>, start_urbe: String, dest_urbe: String, from_v
                     for j in i+1 .. via_len {
                         let next_urbe = &via.urbes[j];
                         cursteps += 1;
-                        for _dp in 0 .. depth { print!("  "); }
+                        for _dp in 0 .. depth { print!("   "); }
                         println!("STEP {} - NX: {} [{} - {}]", cursteps, via.nomen, next_urbe.nomen, next_urbe.miliarium);
                         if next_urbe.nomen == dest_urbe {
                             println!("FOUND DEST!!!\n");
@@ -69,7 +54,7 @@ fn find_path(vrs: &Vec<viae::Via>, start_urbe: String, dest_urbe: String, from_v
                     for j in (0 .. i).rev() {
                         let prev_urbe = &via.urbes[j];
                         cursteps += 1;
-                        for _dp in 0 .. depth { print!("  "); }
+                        for _dp in 0 .. depth { print!("   "); }
                         println!("STEP {} - PV: {} [{} - {}]", cursteps, via.nomen.clone(), prev_urbe.nomen, prev_urbe.miliarium);
                         if via.nomen == dest_urbe {
                             println!("FOUND DEST!!!\n");
